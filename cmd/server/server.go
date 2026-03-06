c2d08551c86cb3cb23408523d4dfbe55d4556a24
Randy Reddig <randy@alta.software>
2026-03-06 12:08:01 -0800

    all: update to latest quic-go

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -4,17 +4,14 @@ import (
 	"context"
 	"crypto/tls"
 	"flag"
-	"fmt"
-	"io"
 	"log"
 	"os"
-	"path/filepath"
 	"time"
 
 	"github.com/alta/swift-quic-datagram-example/internal/insecure"
-	quic "github.com/lucas-clemente/quic-go"
-	"github.com/lucas-clemente/quic-go/logging"
-	"github.com/lucas-clemente/quic-go/qlog"
+
+	quic "github.com/quic-go/quic-go"
+	"github.com/quic-go/quic-go/qlog"
 )
 
 func main() {
@@ -28,6 +25,9 @@ func main() {
 }
 
 func serverMain(addr string) error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	cert, _, err := insecure.LocalCertPool(addr)
 	if err != nil {
 		return err
@@ -45,15 +45,7 @@ func serverMain(addr string) error {
 
 	qlogDir := os.Getenv("QUIC_LOG_DIRECTORY")
 	if qlogDir != "" {
-		quicConfig.Tracer = qlog.NewTracer(func(_ logging.Perspective, connID []byte) io.WriteCloser {
-			fn := filepath.Join(qlogDir, fmt.Sprintf("server-%x.qlog", connID))
-			f, err := os.Create(fn)
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Printf("Created qlog file: %s", fn)
-			return f
-		})
+		quicConfig.Tracer = qlog.DefaultConnectionTracer
 	}
 
 	listener, err := quic.ListenAddr(addr, tlsConfig, quicConfig)
@@ -64,32 +56,32 @@ func serverMain(addr string) error {
 	log.Printf("Server listening at: %s", addr)
 
 	for {
-		conn, err := listener.Accept(context.Background())
+		conn, err := listener.Accept(ctx)
 		if err != nil {
 			return err
 		}
-		go func(conn quic.Connection) {
+		go func(conn *quic.Conn) {
 			log.Printf("QUIC connection started: %s %t",
 				conn.RemoteAddr().String(), conn.ConnectionState().SupportsDatagrams)
-			time.Sleep(1 * time.Second)
-			err := conn.SendMessage([]byte{})
+			time.Sleep(1 * time.Second) // Why?
+			err := conn.SendDatagram([]byte{})
 			if err != nil {
-				log.Printf("Error: SendMessage: %v", err)
+				log.Printf("Error: SendDatagram: %v", err)
 				return
 			}
 			for {
-				buf, err := conn.ReceiveMessage()
+				buf, err := conn.ReceiveDatagram(ctx)
 				if err != nil {
-					log.Printf("Error: ReceiveMessage: %v\n", err)
+					log.Printf("Error: ReceiveDatagram: %v\n", err)
 					break
 				}
-				log.Printf("ReceiveMessage: %s\n", string(buf))
-				err = conn.SendMessage(buf)
+				log.Printf("ReceiveDatagram: %s\n", string(buf))
+				err = conn.SendDatagram(buf)
 				if err != nil {
-					log.Printf("Error: SendMessage: %v", err)
+					log.Printf("Error: SendDatagram: %v", err)
 					break
 				}
-				log.Printf("SendMessage: %s\n", string(buf))
+				log.Printf("SendDatagram: %s\n", string(buf))
 			}
 		}(conn)
 	}
